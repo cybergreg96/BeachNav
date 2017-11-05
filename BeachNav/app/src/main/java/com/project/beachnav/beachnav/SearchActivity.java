7b4f03c9015229a4f051cc39756473c1768c71a1
MrAlphalfa97 <32249785+MrAlphalfa97@users.noreply.github.com>
2017-11-04 21:51:41 -0700

    small changes
    added node.java file, and a few ui changes

--- BeachNav/app/src/main/java/com/project/beachnav/beachnav/SearchActivity.java
+++ BeachNav/app/src/main/java/com/project/beachnav/beachnav/SearchActivity.java
@@ -6,20 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 /**
  * Created by Austin on 10/25/2017.
  */
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchResultsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps_searchbar);
-        Toolbar menu_toolbar = (Toolbar) findViewById(R.id.menu_toolbar);
-        setSupportActionBar(menu_toolbar);
+        setContentView(R.layout.activity_result);
         handleIntent(getIntent());
     }
 
@@ -41,7 +38,6 @@ public class SearchActivity extends AppCompatActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             //use query to search
-            //cycle back to MapActivity?
         }
     }
 }
