1fc8aed1c118d25528738bec3e97cff6e427f0ab
Paper-Hat <justinkristian@gmail.com>
2017-11-13 11:31:58 -0800

    More minor changes
    Added documentation, removed getLabel parameter as it is unnecessary for what we're doing with the method.

--- BeachNav/app/src/main/java/com/project/beachnav/beachnav/other/PathToDraw.java
+++ BeachNav/app/src/main/java/com/project/beachnav/beachnav/other/PathToDraw.java
@@ -11,23 +11,26 @@ import java.util.ArrayList;
  */
 
 public class PathToDraw {
+    /* Void method drawPath iterates through the shortestPath to generate a Location ArrayList using
+    * Node 'x' and 'y' values for latitude and longitude coordinates. Method then creates a Polyline
+    * object to draw lines between the locations, later adding it to the Google Map Object*/
     public void drawPath(ArrayList<Node> shortestPath, GoogleMap gmap)
     {
         Node[] nodeArr = (Node[])shortestPath.toArray();
         ArrayList<Location> drawnLocs = new ArrayList<Location>();
-        PolylineOptions lineToDraw = new PolylineOptions();
+        PolylineOptions lineToDraw = new PolylineOptions(); //Line object compatible with locations
         lineToDraw.color(Color.parseColor("#CC0000FF"));
         lineToDraw.width(5);
         lineToDraw.visible(true);
-        for(int i=0;i<nodeArr.length;i++){
-            Location toAdd = new Location(nodeArr[i].getLabel());
-            toAdd.setLatitude(nodeArr[i].getX());
-            toAdd.setLongitude(nodeArr[i].getY());
+        for(int i=0;i<nodeArr.length;i++){ //Iterates through nodeArr to create Location ArrayList
+            Location toAdd = new Location(""); //Provider is unnecessary for this method
+            toAdd.setLatitude(nodeArr[i].getX()); //uses Node x value for latitude
+            toAdd.setLongitude(nodeArr[i].getY()); //uses Node y value for longitude
             drawnLocs.add(toAdd);
         }
-        for(Location drawn: drawnLocs){
+        for(Location drawn: drawnLocs){ //Draws through LatLng objects created from location arrlist
             lineToDraw.add(new LatLng(drawn.getLatitude(), drawn.getLongitude()));
         }
-        gmap.addPolyline(lineToDraw);
+        gmap.addPolyline(lineToDraw); //adds lines to visible map
     }
 }
